41ce27b76bcf737405906708bc3fa38ae3475f38
josiesanti <josiesanti@yahoo.com>
2014-04-11 19:05:34 -0400

    Regex altered

--- Parser.py
+++ Parser.py
@@ -1,9 +1,7 @@
 import re
-recipe = ''
+recipe = '4.0 sm Acorn squash 0.0 Salt 118.3mlButter or margarine 118.3 ml Honey 453.6 g Whole-berry cranberry sauce'
 
 def parseRecipe(recipe):
-    recipeFile = open("rec1.txt", "r") #recipeFile gets file object
-    recipe = recipeFile.read() #recipe gets string object
     #regex for value and unit of measurement
     unitEx = re.compile('(?:(?:\d+(?:\|/|.)?\d+)\s*(?:ounces?|oz|pounds?|lbs?|' 
                     + 'milligrams?|mg|grams?|g|kilograms?|kg|'
@@ -12,7 +10,7 @@ def parseRecipe(recipe):
                     + 'fahrenheit|f))', re.IGNORECASE)
     #creates a list of all substrings matching regex
     celist = re.findall(unitEx, recipe)    
-   
+    print(celist)
     #regex to match first occurence of appropriate alphabetic character
     Ex = re.compile('[oplcmgkif]', re.IGNORECASE)
     #each string element in celist gets converted into
@@ -24,9 +22,10 @@ def parseRecipe(recipe):
         end = ce.__len__()
         value = float(ce[0:start])# takes double part of string and assigns it to value
         unit = ce[start:end]#takes measurement unit par of string and assigns it to unit
-        conEl = ConvertibleElement(value,unit)
-        self.setCElistElement(conEl)
-       
+##        conEl = ConvertibleElement(value,unit)
+##        self.setCElistElement(conEl)
+
+        
     # setParsedRecipe(recipe):   
     #Replaces each convertable element with <#>
     num = 0
@@ -34,6 +33,4 @@ def parseRecipe(recipe):
         recipe = recipe.replace(ce, '<'+ str(num) + '>',1)
         num = num + 1
     print(recipe)
-    
-
-    
+  
