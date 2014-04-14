192959aa2a05c6689c644c7a29c323ff0b5dbac7
josephine santiago <josiesanti@yahoo.com>
2014-04-14 01:04:54 -0500

    Created two separate methods for portion of parser that adds tags to convertible elements and portion of parser that creates a convertible element object. parseRecipe method will call these methods.

--- Parser.py
+++ Parser.py
@@ -1,7 +1,6 @@
 import re
 #testable string
 recipe = '4.0 sm Acorn squash 0.0 Salt 118.3mlButter or margarine 118.3 ml Honey 453.6 g Whole-berry cranberry sauce'
-
 def parseRecipe(recipe):
     #regex for value and unit of measurement
     unitEx = re.compile('(?:(?:\d+(?:\|/|.)?\d+)\s*(?:ounces?|oz|pounds?|lbs?|' 
@@ -14,23 +13,26 @@ def parseRecipe(recipe):
     print(celist)
     #regex to match first occurence of appropriate alphabetic character
     Ex = re.compile('[oplcmgkif]', re.IGNORECASE)
-    #each string element in celist gets converted into
-    #convertible elements(calls convertible element constructor)
-    #and gets modified element and places it in list
-    for ce in celist:        
-        m = re.search(Ex,ce)#recognizes where the original string should split
+    createConEl(celist,Ex)
+    addTags(celist,recipe)
+    
+#each string element in celist gets converted into
+#convertible elements(calls convertible element constructor)
+#and gets modified element and places it in list    
+def createConEl(list, regex):    
+    for ce in list:        
+        m = re.search(regex,ce)#recognizes where the original string should split
         start= m.start()
         end = ce.__len__()
         value = float(ce[0:start])# takes double part of string and assigns it to value
-        unit = ce[start:end]#takes measurement unit par of string and assigns it to unit
+        unit = ce[start:end]#takes measurement unit par of string and assigns it to unit            
 ##        conEl = ConvertibleElement(value,unit)
 ##        self.setCElistElement(conEl)
 
-        
-    # setParsedRecipe(recipe):   
-    #Replaces each convertable element with <#>
+#Replaces each convertable element with <#>
+def addTags(list,recipe): 
     num = 0
-    for ce in celist:
+    for ce in list:
         recipe = recipe.replace(ce, '<'+ str(num) + '>',1)
         num = num + 1
     print(recipe)
