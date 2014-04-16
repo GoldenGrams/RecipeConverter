f887951c218f9f5e57cc751f488fa11ce86ce32a
josiesanti <josiesanti@yahoo.com>
2014-04-16 01:33:37 -0400

    Converts strings containing numerical value to double See convertValue method

--- Parser.py
+++ Parser.py
@@ -1,9 +1,9 @@
 import re
 #testable string
-recipe = '1/2 cup 4.0 sm Acorn squash 0.0 Salt 118.3mlButter or margarine 118.3 ml Honey 453.6 g Whole-berry cranberry sauce 5.5 liters'
+recipe = '500 18/6 cups 4.0 sm Acorn squash 0.0 Salt 118.3mlButter or margarine 118.3 ml Honey 453.6 g Whole-berry cranberry sauce'
 def parseRecipe(recipe):
     #regex for value and unit of measurement
-    unitEx = re.compile( '''(?:(?:\d+(?:\|/|.)?\d*)\s*(?:ounces?|oz|pounds?|lbs?
+    unitEx = re.compile( '''(?:(?:\d*\s*\d+(?:/|.)?\d*)\s*(?:ounces?|oz|pounds?|lbs?
                           |fluid\s*ounces?|milligrams?|mg|grams?|g|kilograms?|kg 
                           |fl\s*oz|milliliters?|ml|liters?|l|inches|inch|in|pints?
                           |millimeters?|mm|centimeters?|quarts?|qt|cm|cups?))|
@@ -13,28 +13,64 @@ def parseRecipe(recipe):
     celist = re.findall(unitEx, recipe)    
     print(celist)
     #regex to match first occurence of appropriate alphabetic character
-    Ex = re.compile('[oplcmgkif]', re.IGNORECASE)
+    Ex = re.compile('[oplcmgkifº]', re.IGNORECASE)
     createConEl(celist,Ex)
     addTags(celist, recipe)
     
 #each string element in celist gets converted into
 #convertible elements(calls convertible element constructor)
 #and gets modified element and places it in list    
-def createConEl(list, regex):    
-    for ce in list:        
+def createConEl(list, regex):
+    for ce in list:
         m = re.search(regex,ce)#recognizes where the original string should split
         start= m.start()
         end = ce.__len__()
-        value = float(ce[0:start])# takes double part of string and assigns it to value
-        unit = ce[start:end]#takes measurement unit par of string and assigns it to unit            
+        strvalue = ce[0:start]# takes double part of string and assigns it to value
+        print(strvalue)
+        double = convertValue(strvalue)
+        print(double)                
+        unit = ce[start:end]#takes measurement unit part of string and assigns it to unit                      
+        if (unit == 'fahrenheit' or'ºf'or'f'):
+            unit = 'degrees f'
+            print(unit)
+        elif (unit == 'celsius' or 'ºc' or 'c'):
+            unit = 'degrees c'
+            print(unit)
+        else:
+            print(unit)
 ##        conEl = ConvertibleElement(value,unit)
 ##        self.setCElistElement(conEl)
 
-#Replaces each convertable element with <#>
+#Converts string into double. Returns double   
+def convertValue(strvalue):
+    #if string contains whole number or decimal. gets converted easily
+    if(strvalue.find('/')==-1):
+           value = float(strvalue)
+    #converts string containing fraction into double
+    else:
+        #if string does not contain white space, only deal with fraction
+        if(strvalue.find(' ')==-1):
+            numden = strvalue.split('/')#holding numerator/denominator
+            num = float(numden.pop(0))
+            den = float(numden.pop(0))
+            value = (num/den)
+        #if string contains white space, deal with whole number and fraction
+        else:
+            white = strvalue.find(' ')
+            fnum = strvalue[0:white]
+            newstr = strvalue[white:strvalue.__len__()]
+            numden = newstr.split('/')
+            num = float(numden.pop(0))
+            den = float(numden.pop(0))
+            value = (float(fnum))+(num/den)
+    return value
+
+#Replaces each convertable element with <#>. Returns recipe with tagged elements
 def addTags(list,recipe): 
     num = 0
     for ce in list:
         recipe = recipe.replace(ce, '<'+ str(num) + '>',1)
         num = num + 1
+    return recipe
     print(recipe)
   
